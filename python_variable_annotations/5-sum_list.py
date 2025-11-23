8e3e75d2eef8352017ccd7ea777bff335f6f986a
Gerdia0117 <Gerald.froz@gmail.com>
2025-11-23 18:59:11 -0400

    syntax mistake  inside of function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Type-Annotated Function sum_list."""
 
-def sum_list(input_list list[float]) -> float:
+from typing import list
+
+def sum_list(input_list: list[float]) -> float:
     """Return sum of all elemenets in list of floats."""
     return sum(input_list)
