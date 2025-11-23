d57a52b665af8af768bebeb7bab86b7771cded00
Gerdia0117 <Gerald.froz@gmail.com>
2025-11-23 18:36:12 -0400

    Sum list which takes a list of imput list and retun sum as float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Type-Annotated Function sum_list."""
+
+def sum_list(imput_list list[float]) -> float:
+    """Return the sum of all elemenets in a list of floats."""
+    return sum(imput_list)
