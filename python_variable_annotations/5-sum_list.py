fbdf0c7549534f664a6f48052ea82fd2f562322b
Gerdia0117 <Gerald.froz@gmail.com>
2025-11-23 19:06:17 -0400

    Fix typing import in 5-sum_list.py

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function sum_list."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Return the sum of all floats in a list."""
     return sum(input_list)
