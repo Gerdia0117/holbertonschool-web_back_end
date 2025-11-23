5f2ea0ed03c7a5207fdf463d8950c98bfbfdd005
Gerdia0117 <Gerald.froz@gmail.com>
2025-11-23 18:37:18 -0400

    misspelled input

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Type-Annotated Function sum_list."""
 
-def sum_list(imput_list list[float]) -> float:
+def sum_list(input_list list[float]) -> float:
     """Return the sum of all elemenets in a list of floats."""
-    return sum(imput_list)
+    return sum(input_list)
