689c0329ced1331250a8989ba42cd97edc89f556
Gerdia0117 <Gerald.froz@gmail.com>
2025-11-23 18:51:02 -0400

    Fixing and testing error

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,5 +2,5 @@
 """Type-Annotated Function sum_list."""
 
 def sum_list(input_list list[float]) -> float:
-    """Return the sum of all elemenets in a list of floats."""
+    """Return sum of all elemenets in list of floats."""
     return sum(input_list)
