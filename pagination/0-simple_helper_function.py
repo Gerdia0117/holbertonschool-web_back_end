9fe628097dfe0bc231e45ed6a3d2fa164428e111
Gerdia0117 <Gerald.froz@gmail.com>
2025-12-08 13:09:46 -0400

    # in line 3 in simple_helper_function

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-#0-simple_helper_function.py
+# 0-simple_helper_function.py
 
 
 def index_range(page, page_size):
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
-
